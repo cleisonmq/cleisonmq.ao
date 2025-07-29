ec3df1e555840f257eb44b133984f997221fc074
dkunivers <138530949+dkunivers@users.noreply.github.com>
2025-07-29 23:29:55 +0100

    Add files via upload

---
+++ script/design_video.js
@@ -0,0 +1,10 @@
+const open_menu = document.getElementById("hamb_btn");
+const cloused_menu = document.getElementById("closed");
+const menu = document.getElementById("links");
+
+open_menu.onclick = function () {
+    menu.style.display=("block")   
+}
+cloused_menu_menu.onclick = function () {
+    menu.style.display=("none")   
+}
